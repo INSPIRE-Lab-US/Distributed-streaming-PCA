6b67c57f1b51b97efca55cde88ce3f038df4ba86
Haroon Raja <haroonraja86@gmail.com>
2022-10-26 19:57:23 -0400

    Add files via upload

---
+++ HIGGS code/MiniBatchKrasulinaHIGGS_B1.m
@@ -0,0 +1,55 @@
+function [u] = MiniBatchOjaHIGGS_Final_B1
+%% This scripts computes dominant eigenvector using Krasulina's method for
+% mini-batch data settings
+%% Data input
+% tic
+% HiggsData = csvread('HIGGS.csv');
+% toc
+% save -v7.3 HIGGS_mat HiggsData
+ER = load('HIGGS_mat.mat');
+ER = ER.HiggsData;
+ER = ER(:, 2:end)'; % We are discarding the first dimension since it contains only labels
+%% Input paramters
+Batch = 1;
+dataDimension = size(ER, 1);
+TotalSamples = size(ER, 2);
+TotalIterations = TotalSamples;
+gamma_vary = 2e3:1e3:6e3;
+ErrorMiniBatchOja = zeros(TotalIterations + 1, length(gamma_vary));
+TotalConvergingRuns = zeros(length(gamma_vary), 1);
+a = mean(ER, 2);
+ER = ER - repmat(a, [1 size(ER, 2)]);
+ER_1 = ER/max(max(ER));
+%% Centralized Solution
+[U, ~, ~] = svds(ER_1, 1);
+u_centralized = U(:,1);
+TotalSimulationRuns = 200;
+for simulationRun=1:TotalSimulationRuns
+    tic
+    rng(simulationRun)
+    ER = ER_1(:, randperm(size(ER, 2)));
+    x(:, 1) = randn(dataDimension, 1);
+    x(:, 1) = x(:, 1)/norm(x(:, 1));
+    for i=1:length(gamma_vary)
+        x_t = x(:, 1);
+        ErrorMini = zeros(TotalIterations + 1, 1);
+        ErrorMini(1, 1) = 1 - (x(:, 1)'*u_centralized)^2/norm(x(:, 1))^2;
+        batch = Batch;
+        for k=1:TotalIterations/batch
+            CovarianceBatch = (1/batch)*ER(:, (k-1)*batch+1:k*batch)*ER(:, (k-1)*batch+1:k*batch)';
+            x_t = x_t + ((gamma_vary(i))/k)*(CovarianceBatch*x_t - (x_t'*CovarianceBatch*x_t)*x_t/(x_t'*x_t));
+            ErrorMini((k-1)*batch+2:k*batch+1) = 1 - (x_t'*u_centralized)^2/norm(x_t)^2;
+        end
+        if ~sum(isnan(ErrorMini))
+            ErrorMiniBatchOja(:, i) = ErrorMiniBatchOja(:, i)+ErrorMini;
+            TotalConvergingRuns(i) = TotalConvergingRuns(i) + 1;
+        end
+    end
+    disp(simulationRun)
+    toc
+    %% Saving results
+    tic
+    save -v7.3 ErrorKrasulinaMiniBatch_Final_B_1 ErrorMiniBatchOja TotalConvergingRuns
+    toc
+end
+end
---
+++ HIGGS code/MiniBatchKrasulinaHIGGS_B1000_Latency.m
@@ -0,0 +1,56 @@
+function [u] = MiniBatchOjaHIGGS_Final_B1000_Latency
+%% This scripts computes dominant eigenvector using Krasulina's method for
+% mini-batch data settings
+%% Data input
+% tic
+% HiggsData = csvread('HIGGS.csv');
+% toc
+% save -v7.3 HIGGS_mat HiggsData
+ER = load('HIGGS_mat.mat');
+ER = ER.HiggsData;
+ER = ER(:, 2:end)'; % We are discarding the first dimension since it contains only labels
+%% Input paramters
+Batch = 1000;
+dataDimension = size(ER, 1);
+TotalSamples = size(ER, 2);
+TotalIterations = TotalSamples;
+gamma = 6000;
+Latency = [0, 10, 100, 1000, 5000, 1e4];
+ErrorMiniBatchOja = zeros(TotalIterations + 1, length(Latency));
+TotalConvergingRuns = zeros(length(Latency), 1);
+a = mean(ER, 2);
+ER = ER - repmat(a, [1 size(ER, 2)]);
+ER_1 = ER/max(max(ER));
+%% Centralized Solution
+[U, ~, ~] = svds(ER_1, 1);
+u_centralized = U(:,1);
+TotalSimulationRuns = 200;
+for simulationRun=1:TotalSimulationRuns
+    tic
+    rng(simulationRun)
+    ER = ER_1(:, randperm(size(ER, 2)));
+    x(:, 1) = randn(dataDimension, 1);
+    x(:, 1) = x(:, 1)/norm(x(:, 1));
+    for i=1:length(Latency)
+        x_t = x(:, 1);
+        ErrorMini = zeros(TotalIterations + 1, 1);
+        ErrorMini(1, 1) = 1 - (x(:, 1)'*u_centralized)^2/norm(x(:, 1))^2;
+        batch = Batch;
+        for k=1:TotalIterations/(Batch+Latency(i))
+            CovarianceBatch = (1/batch)*ER(:, (k-1)*batch+1:k*batch)*ER(:, (k-1)*batch+1:k*batch)';
+            x_t = x_t + (gamma/k)*(CovarianceBatch*x_t - (x_t'*CovarianceBatch*x_t)*x_t/(x_t'*x_t));
+            ErrorMini((k-1)*(Batch+Latency(i))+2:k*(Batch+Latency(i))+1) = 1 - (x_t'*u_centralized)^2/norm(x_t)^2;
+        end
+        if ~sum(isnan(ErrorMini))
+            ErrorMiniBatchOja(:, i) = ErrorMiniBatchOja(:, i)+ErrorMini;
+            TotalConvergingRuns(i) = TotalConvergingRuns(i) + 1;
+        end
+    end
+    disp(simulationRun)
+    toc
+    %% Saving results
+    tic
+    save -v7.3 ErrorKrasulinaMiniBatch_Final_B_1000_Latency ErrorMiniBatchOja TotalConvergingRuns
+    toc
+end
+end
